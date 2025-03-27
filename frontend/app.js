13e9718f671baa5d6eb7170bb0a7da278d086457
Sayanth M <116065362+sayu585@users.noreply.github.com>
2025-03-27 11:08:47 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,6 @@
+async function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    alert(`Login Attempt: ${email}`);
+}
