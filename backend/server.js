09c308cfd109cc1884bd9a06c76a0c66c15ae779
Sayanth M <116065362+sayu585@users.noreply.github.com>
2025-03-27 15:55:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const PORT = 5000;
 
 app.use(express.json());
 
+// Authentication routes
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
     res.send("Kulstock CRM Backend is Running...");
 });
