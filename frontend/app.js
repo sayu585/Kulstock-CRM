a8cabc29b35104cad55869297d68bb30e1a94f23
Sayanth M <116065362+sayu585@users.noreply.github.com>
2025-03-27 14:45:40 +0530

    Delete frontend/app.js

--- frontend/app.js
+++
@@ -1,6 +0,0 @@
-async function login() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    alert(`Login Attempt: ${email}`);
-}
