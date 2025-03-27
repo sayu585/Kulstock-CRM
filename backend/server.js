c1afe079bee7d46c079aca2a2c728c0cdab64e86
Sayanth M <116065362+sayu585@users.noreply.github.com>
2025-03-27 11:02:42 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
